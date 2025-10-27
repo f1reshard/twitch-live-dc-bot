3b9d984953b82d06d885d9cdad05f992e56b6b07
f1reshard <dsorlander@gmail.com>
2025-10-27 04:19:03 +0100

    adding title change updates functionality

--- i have no idea.py
+++ i have no idea.py
@@ -10,6 +10,8 @@ from dotenv import load_dotenv
 from ast import literal_eval
 import threading
 from requests.exceptions import RequestException
+from datetime import datetime
+from discord import Embed
 
 
 load_dotenv(dotenv_path='twitch_clients.env')
@@ -27,6 +29,7 @@ user_threads = {}
 user_stop_events = {}
 # Track live status in memory only
 live_status = {k: False for k in users}
+current_titles = {k: "" for k in users}
 
 # Add a lock for thread-safe operations
 thread_lock = threading.Lock()
@@ -49,7 +52,7 @@ def get_token():
     response = requests.post(url, params=params)
     return response.json()['access_token']
 
-def get_user_info(headers, livename, stop_event=None):
+def get_user_info(headers, livename, stop_event=None, skip_sleep=False):
     global userinfo, token
     while True:
         if stop_event and stop_event.is_set():
@@ -59,9 +62,11 @@ def get_user_info(headers, livename, stop_event=None):
         response = requests.get(f'https://api.twitch.tv/helix/users?login={livename}', headers=headers)
         if response.status_code in [200, 201]:
             userinfo[livename] = response.json()['data'][0]
-
-            if not debug:
+            if not debug and skip_sleep == False:
                 sleep(1800)
+            if skip_sleep:
+                break
+
         elif response.status_code == 401:
             token = get_token()
             headers['Authorization'] = f'Bearer {token}'
@@ -69,10 +74,10 @@ def get_user_info(headers, livename, stop_event=None):
         else:
             fprint(f'Error {response.status_code}')
 
-def get_live_info(headers, livename):
+def get_live_info(headers, livename, apicall):
     global token
     try:
-        response = requests.get(f'https://api.twitch.tv/helix/streams?user_login={livename}', headers=headers)
+        response = requests.get(f'https://api.twitch.tv/helix/{apicall}', headers=headers)
         if response.status_code in [200, 201]:
             return response
 
@@ -82,15 +87,17 @@ def get_live_info(headers, livename):
             return get_live_info(headers, livename)
         else:
             fprint(f'Error {response.status_code}')
-            return None
+            return 'error'
     except RequestException as e:
         fprint(f'Error in get_live_info: {e}')
-    
+        return 'error'
 
 def send_discord_embed(livename, islive):
+
     while livename not in userinfo:
         fprint(f"Waiting for user info for {livename}...")
         sleep(1)
+
     if islive:
         content = f"<@267440142124449794> {userinfo[livename]['display_name']} is [live!](https://twitch.tv/{livename})"
     else:
@@ -113,11 +120,15 @@ def check_live(headers, livename, stop_event=None):
                 )
                 webhook.execute()
                 break
-                
-            response = get_live_info(headers, livename)
+
+            #####################################
+            response = get_live_info(headers, livename, f'streams?user_login={livename}')
             is_live = False
 
-            if response and response.json()['data']:
+            if response == 'error':
+                continue
+
+            elif response and response.json()['data']:
                 if response.json()['data'][0]['type'] == 'live':
                     is_live = True
 
@@ -132,19 +143,81 @@ def check_live(headers, livename, stop_event=None):
                     send_discord_embed(livename, False)
 
             live_status[livename] = is_live
+            ######################################
+
+            titleupdate = stream_title(livename, headers)
+
+            if titleupdate != 'error' and titleupdate != current_titles[livename]:
+                fprint(f"[blue]{livename} updated stream title to: {titleupdate}[/blue]")
+                current_titles[livename] = titleupdate
+            
+                embed = {
+                "author": {
+                    "name": f"{userinfo[livename]['display_name']}",
+                    "icon_url": f"{userinfo[livename]['profile_image_url']}",
+                    "url": f"https://twitch.tv/{livename}"
+                },
+                "title": "Title Change",
+                "fields": [
+                    {
+                    "name": f"{titleupdate}",
+                    "value": "\u200b",
+                    "inline": False
+                    }
+                ],
+                "color": 0x359e69,
+                "footer": {
+                    "text": "Time"
+                },
+                "timestamp": datetime.utcnow().isoformat()}
+
+                webhook = DiscordWebhook(
+                    url=os.getenv('webhook_url'),
+                )
+                webhook.add_embed(embed)
+                webhook.execute()
+
+
+
             if not debug:
                 sleep(5)
     except Exception as e:
         fprint(f"[red]check_live thread for {livename} crashed: {e}")
 
+def stream_title(livename, headers):
+    try:
+        if livename not in userinfo:
+            get_user_info(headers, livename, skip_sleep=True)
+
+        response = requests.get(f'https://api.twitch.tv/helix/channels?broadcaster_id={userinfo[livename]["id"]}', headers=headers)
+
+        if response.status_code in [200, 201]:
+            usertitle = response.json()['data'][0]['title']
+            return usertitle
+
+        elif response.status_code == 401:
+            token = get_token()
+            headers['Authorization'] = f'Bearer {token}'
+            return stream_title(livename, headers)
+        
+        elif response.status_code == 429:
+            fprint('[red]Rate limited when fetching stream title')
+            sleep(1)
+
+    except Exception as e:
+        fprint(f'Error in stream_title: {e}')
+        return 'error'
+
 @bot.command()
 async def adduser(ctx, username):
+    global headers
     with thread_lock:
         if username in users:
             await ctx.send(f'{username} already exists')
             return
         users[username] = False
         live_status[username] = False
+        current_titles[username] = stream_title(username, headers)
         with open('users.txt','w') as q:
             q.write(str(users))
         # Create stop event and threads for new user
@@ -178,6 +251,8 @@ async def removeuser(ctx, username):
                 del user_threads[username]
             if username in user_stop_events:
                 del user_stop_events[username]
+            if username in current_titles:
+                del current_titles[username]
 
             with open('users.txt','w') as q:
                 q.write(str(users))
@@ -271,5 +346,6 @@ if __name__ == "__main__":
             t2.start()
             user_threads[x] = [t1, t2]
             live_status[x] = False
+            current_titles[x] = stream_title(x, headers)
     if not debug:
         bot.run(token=dcbot_token)
